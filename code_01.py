9b9d9c38bf78c6df7dd07155b2e2c94170f7f628
Manish <manish.fluffycodes@gmail.com>
2020-11-04 16:47:57 +0530

    Initial commit

---
+++ code_01.py
@@ -0,0 +1,15 @@
+# Pratical Q1 Print a Poem
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+''')
+
+''' Note: if we are using a string which
+is longer than one line like the Poem than
+we should use triple quote like used in this
+comment or shown above in the code but use
+use the function to execute as the triple
+qoute used without any function becomes
+multi-line comment (like this comment!!!)'''
