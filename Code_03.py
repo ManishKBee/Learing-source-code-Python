dbdb4cd47cf40dee4e8f51426853defe1bf79066
Manish <manish.fluffycodes@gmail.com>
2020-11-04 17:22:04 +0530

    Modules, Comments and pip

---
+++ Code_03.py
@@ -0,0 +1,13 @@
+# Pratical Q3 import any external module an execute
+
+from playsound import playsound
+playsound('D:\\Python Program Codes\\Chapter 1\\play.mp3')
+
+'''Note: we have downloaded an external
+module named playsound which enable us to play
+music using python by just giving the path
+of mp3 or any audio file!!! used double '\\'
+coz if we use single ('\' it's an escape sequence) 
+then it direct or send signals to python that 
+after this symbol move all other part of the to 
+next line''' 
---
+++ Code_04.py
@@ -0,0 +1,13 @@
+# List all the contents in a directory using os module
+# Author: FluffyBee
+# Date and Location: 20.10.2020 and NNCM05
+
+import os
+print(os.listdir())
+
+'''Note: have some patience and try again 
+there must be some silly mistake, take a 
+look on the code and try angain running the 
+code with some changes or just rewrite the 
+code with keeping clear note of where does 
+it went wrong in previous attempt'''
