29eefb871452d79141b0d43671249978b8d55251
Manish <manish.fluffycodes@gmail.com>
2020-11-08 18:16:32 +0530

    List and Tuples

---
+++ List and Tuples/05_tuples_methods.py
@@ -0,0 +1,10 @@
+# different methods/function in tuples
+
+dot=(43,45,67,23,41,98,45,34,69,45,67,89,23,60,20,45)
+print(dot.count(45))
+# this will return the number of occurrences of a particular value which is asked like .count(34) then it will count the total num of occurrences of num "34" in the tuple
+
+print(dot.index(45))
+# this returns the index num of a specific num like if our tuple is (1,2,3,4,5,6,7,5,8,6,5) and we use .index(5) then python will return the indexed position of "num 5" which is "4" as the index starts with '0' and it will only show the first "occurrences of the indexed position" as we have seen in the example
+
+# for more tuple methods vist python.org
---
+++ List and Tuples/Practice_02.py
@@ -0,0 +1,16 @@
+# Q2. python code to record marks and sort the marks
+
+# 'S' stands for Students 
+
+S1=int(input("Enter The Marks of S1: "))
+S2=int(input("Enter The Marks of S2: "))
+S3=int(input("Enter The Marks of S3: "))
+S4=int(input("Enter The Marks of S4: "))
+S5=int(input("Enter The Marks of S5: "))
+S6=int(input("Enter The Marks of S6: "))
+
+List_of_Marks=[S1,S2,S3,S4,S5,S6]
+
+List_of_Marks.sort()
+
+print(List_of_Marks)
