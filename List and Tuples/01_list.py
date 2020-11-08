8bbc95fbbe7c3c82f06bc65d10f4a46a88c0ecae
Manish <manish.fluffycodes@gmail.com>
2020-11-08 08:46:34 +0530

    List and Tuples

---
+++ List and Tuples/01_list.py
@@ -0,0 +1,19 @@
+# python list are container to store a set of value of "ANY DATA TYPE"
+# the value stored in the list are arranged in a specific "ORDER"
+
+a=[56,34,76,23,67]
+# create a list using "[]"
+
+print(a)
+# print the list index using print()function
+
+print(a[4])
+# access list index using a[0],a[3],a[1] till the indexed num of the letter
+
+a[3]=89
+# changethe value of list using a[index num i.e, 0,1,2,till the last indexed num]=the value like to change(12,45,87,etc...)
+print(a)
+
+b=["fluffy",34,69.69,True]
+# we can create a list with item of different data type
+print(b)
